693cda7f026f26c593e98897bb2f366b865775a5
Lumion Technology <lumiontechnnology@gmail.com>
2026-01-29 03:54:11 +0100

    Update authRoutes.ts

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,1 +1,10 @@
+import { Router } from 'express';
+import authController from '../controllers/auth.controller';
+import { authLimiter } from '../middleware/rateLimiter';
 
+const router = Router();
+
+router.post('/register', authLimiter, authController.register.bind(authController));
+router.post('/login', authLimiter, authController.login.bind(authController));
+
+export default router;
